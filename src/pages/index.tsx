5609dc959ac47e5e3c9ed999e6c25642e1d29de1
Carlos Alexandre <alexfilho062005@gmail.com>
2025-05-14 08:59:22 -0300

    :heavy_plus_sign: add shadcnui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import { Button } from '@/components/ui/button';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { cn } from '@/lib/utils';
+const geistSans = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist-sans',
+});
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+});
+
 export default function Home() {
   return (
-    <div className="bg-red-600">
-      <h1>Hello World</h1>
+    <div
+      className={cn(
+        'min-h-screen bg-background font-sans antialiased',
+        geistSans.variable,
+        geistMono.variable,
+      )}
+    >
+      <Button variant="secondary">Hello World</Button>
     </div>
   );
 }
