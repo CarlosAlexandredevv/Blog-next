88b0de05a0881e231e847692168724b1fb28ab7e
Carlos Alexandre <alexfilho062005@gmail.com>
2025-05-14 10:22:26 -0300

    :lipstick: feat: create footer component

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,5 +1,5 @@
 import { Header } from '../header';
-
+import { Footer } from '../footer';
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -9,6 +9,7 @@ export function Layout({ children }: LayoutProps) {
     <div className="relative flex min-h-screen flex-col dark">
       <Header />
       <main className="flex-1 flex flex-col mb-12">{children}</main>
+      <Footer />
     </div>
   );
 }
