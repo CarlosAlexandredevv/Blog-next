ad8f537fc7971e5fbf3de753dc3173bd0bd0ae7d
Carlos Alexandre <alexfilho062005@gmail.com>
2025-10-28 21:18:55 -0300

    :recycle: refactor: cards customer action

--- src/components/templates/landing-page/sections/customer-story-section/customer-story-section.tsx
+++ src/components/templates/landing-page/sections/customer-story-section/customer-story-section.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import { CardCustomerSection } from '@/components/card-customer-section';
 
 const customerStories = [
   {
@@ -31,34 +31,10 @@ export function CustomerStorySection() {
 
         <div className="grid gap-8 md:grid-cols-2">
           {customerStories.map((customerStory) => (
-            <div
+            <CardCustomerSection
               key={customerStory.author.name}
-              className="flex flex-col gap-6 rounded-lg bg-gray-500 p-6 md:p-12"
-            >
-              <p className="text-balance text-gray-200">
-                {customerStory.content}
-              </p>
-
-              <div className="flex items-center gap-3">
-                <div className="relative h-10 w-10 overflow-hidden rounded-full">
-                  <Image
-                    src={customerStory.author.avatar}
-                    alt={customerStory.author.name}
-                    fill
-                    className="object-cover"
-                  />
-                </div>
-
-                <div className="flex flex-col">
-                  <strong className="text-gray-200 text-sm">
-                    {customerStory.author.name}
-                  </strong>
-                  <span className="text-xs text-gray-300">
-                    {customerStory.author.role}
-                  </span>
-                </div>
-              </div>
-            </div>
+              {...customerStory}
+            />
           ))}
         </div>
       </div>
--- src/components/templates/landing-page/sections/support-section/support-section.tsx
+++ src/components/templates/landing-page/sections/support-section/support-section.tsx
@@ -2,6 +2,32 @@ import { HeartHandshake, PaintbrushVertical, Store } from 'lucide-react';
 import { CardSupportSection } from '@/components/card-support-section';
 
 export function SupportSection() {
+  const cardsSupport = [
+    {
+      title: 'Personalize seu site',
+      paragraph:
+        'Adicione sua logo, favicon, cores no seu catálogo e tenha tudo com a sua cara.',
+      icon: <PaintbrushVertical className="h-6 w-6 text-white" />,
+      bgCard: 'bg-blue-400',
+      bgIcon: 'bg-blue-300',
+    },
+    {
+      title: 'Venda de qualquer loja',
+      paragraph:
+        'Não importa a loja, o Site.Set permite que você insira qualquer link de afiliado.',
+      icon: <Store className="h-6 w-6 text-white" />,
+      bgCard: 'bg-cyan-300',
+      bgIcon: 'bg-cyan-200',
+    },
+    {
+      title: 'Receba suporte amigável',
+      paragraph:
+        'Nossa equipe estará sempre pronta para te atender para ajudar no que for preciso.',
+      icon: <HeartHandshake className="h-6 w-6 text-white" />,
+      bgCard: 'bg-blue-400',
+      bgIcon: 'bg-blue-300',
+    },
+  ];
   return (
     <section className="relative pb-8 md:py-10 bg-gray-700">
       <div className="absolute inset-0 hidden md:block bg-[url('/background-features.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
@@ -12,32 +38,9 @@ export function SupportSection() {
         </h2>
 
         <div className="grid gap-6 md:grid-cols-3">
-          {/* Primeiro Card */}
-          <CardSupportSection
-            title="Personalize seu site"
-            paragraph="Adicione sua logo, favicon, cores no seu catálogo e tenha tudo com a sua cara."
-            icon={<PaintbrushVertical className="h-6 w-6 text-white" />}
-            bgCard="bg-blue-400"
-            bgIcon="bg-blue-300"
-          />
-
-          {/* Segundo Card */}
-          <CardSupportSection
-            title="Venda de qualquer loja"
-            paragraph="Não importa a loja, o Site.Set permite que você insira qualquer link de afiliado."
-            icon={<Store className="h-6 w-6 text-white" />}
-            bgCard="bg-cyan-300"
-            bgIcon="bg-cyan-200"
-          />
-
-          {/* Terceiro Card */}
-          <CardSupportSection
-            title="Receba suporte amigável"
-            paragraph="Nossa equipe estará sempre pronta para te atender para ajudar no que for preciso."
-            icon={<HeartHandshake className="h-6 w-6 text-white" />}
-            bgCard="bg-blue-400"
-            bgIcon="bg-blue-300"
-          />
+          {cardsSupport.map((card) => (
+            <CardSupportSection key={card.title} {...card} />
+          ))}
         </div>
       </div>
     </section>
