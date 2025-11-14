d0c17e7ee685a16405759faec8c2706f2aa4279f
Carlos Alexandre <alexfilho062005@gmail.com>
2025-11-14 17:24:37 -0300

    :sparkles: feat: error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { LandingPage } from '@/components/templates/landing-page/landing-page';
 
 export default function HomePage() {
-  return (
-    <>
-      <LandingPage />
-    </>
-  );
+  return <LandingPage />;
 }
